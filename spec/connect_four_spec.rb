87baafbe003f575e9f34dd75ed0b7d512f4407c9
Himumi <erliansyaha@gmail.com>
2024-04-07 19:12:00 +0900

    test: #create_board to has 100 length

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,14 @@
-require '../lib/connect_four.rb'
+require './lib/connect_four'
+
+describe ConnectFour do
+  subject(:game) { described_class.new() }
+  describe '#create_board' do
+    context 'when initialize class automatically create board' do
+      let(:board) { game.instance_variable_get(:@board) }
+      it 'has 100 lenght' do
+        board_length = board.length
+        expect(board_length).to be 100
+      end
+    end
+  end
+end
