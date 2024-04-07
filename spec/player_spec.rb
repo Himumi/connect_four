f018a8ed338f6cbc1847586e47124dc5a6951ba4
Himumi <erliansyaha@gmail.com>
2024-04-07 20:58:17 +0900

    test: #initialize new player object

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,14 @@
 require './lib/player'
+
+describe Player do
+  describe 'initialize' do
+    context 'when create new player' do
+      subject(:player) { described_class.new('X', 'foo') }
+      it 'takes symbol and name' do
+        symbol = player.instance_variable_get(:@symbol)
+        name = player.instance_variable_get(:@name)
+        expect(symbol && name).not_to be_nil
+      end
+    end
+  end
+end
