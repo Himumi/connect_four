0cb6e2f16bd9377826f44e4aa7cde68d4f701c3f
Himumi <erliansyaha@gmail.com>
2024-04-14 09:36:18 +0900

    refactor: add unicode symbol into players

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require './lib/connect_four.rb'
 require './lib/player.rb'
 
-first = Player.new("X", "foo")
-last = Player.new("O", "him")
+first = Player.new("\u2689", "foo")
+last = Player.new("\u2687", "him")
 
 game = ConnectFour.new(first, last)
 
