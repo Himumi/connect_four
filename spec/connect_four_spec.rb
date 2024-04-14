c6e5c3f993a7973f737a4b9ab38bb141f0fe2ef9
Himumi <erliansyaha@gmail.com>
2024-04-14 10:12:03 +0900

    style: add new line after let in #create_board test

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -7,6 +7,7 @@ describe ConnectFour do
   describe '#create_board' do
     context 'when initialize class automatically create board' do
       let(:board) { game.instance_variable_get(:@board) }
+
       it 'has 100 length' do
         board_length = board.length
         expect(board_length).to be 100
